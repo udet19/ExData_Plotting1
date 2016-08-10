6272b3f92fdca470d6a8b8c70901bfa102e2fb93
udet19 <e.gilenko@operationalrisksolutions.com>
2016-08-10 12:18:45 +0200

    Second plot commit

---
+++ plot2.R
@@ -0,0 +1,35 @@
+# -------------------------Read the data-START--------------------------------
+
+rawdata <- read.table("./data/household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?", stringsAsFactors = FALSE)
+
+# -------------------------Read the data-FINISH-------------------------------
+
+#-------------------------Format date and time-START--------------------------
+
+rawdata$Date <- as.Date(rawdata$Date, "%d/%m/%Y")
+rawdata$Time <- format(strptime(rawdata$Time, "%H:%M:%S"), "%H:%M:%S")
+
+#-------------------------Format date and time-FINISH-------------------------
+
+#-------------------------Subset the dates-START------------------------------
+
+data <- subset(rawdata, Date==as.Date("2007-02-01") |  Date==as.Date("2007-02-02"))
+
+#-------------------------Subset the dates-FINISH-----------------------------
+
+#-------------------------Plot histogram-START--------------------------------
+
+data <- cbind(data, datetime=as.POSIXct(paste(data$Date, data$Time), format="%Y-%m-%d %H:%M:%S"))
+
+plot(data$datetime, data$Global_active_power, type = "l", ylab = "Global Active Power (kilowatts)", xlab = "")
+
+#-------------------------Plot histogram-FINISH-------------------------------
+
+
+#-------------------------Save histogram to PNG-START-------------------------
+
+dev.copy(png, "./plot2.png")
+dev.off()
+
+#-------------------------Save histogram to PNG-START-------------------------
+
